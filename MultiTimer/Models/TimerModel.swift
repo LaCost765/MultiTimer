a6ce0ca5003fba77a5c489c03beba65d96087cd7
Egor <lacost765123@gmail.com>
2021-09-06 12:40:10 +0300

    Add gitignore file

---
+++ MultiTimer/Models/TimerModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimerModel.swift
+//  MultiTimer
+//
+//  Created by Egor on 01.09.2021.
+//
+
+import Foundation
---
+++ MultiTimer/ViewModels/TimerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimerViewModel.swift
+//  MultiTimer
+//
+//  Created by Egor on 02.09.2021.
+//
+
+import Foundation
